41a88aa2c38c51a5dc6856ed37f79e2f3a41ad12
Tim Provencio <tim@provencio.com>
2017-09-08 16:47:09 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Create product object
+            var product1 = new Product{ID=101, Name="Rotten Apple", Price=1.99};
+
+            //Serialize this into string
+            var jsonString = JsonConvert.SerializeObject(product1);
+            System.Console.WriteLine(jsonString);
+
+            //Deserialize the JSON string into a Product object
+            var product2 = JsonConvert.DeserializeObject<Product>(jsonString);
+            System.Console.WriteLine($"The Product ID is {product2.ID}");
+            System.Console.WriteLine($"The Product Name is {product2.Name}");
+            System.Console.WriteLine($"The Product Price is {product2.Price}");
+            
+        }
+    }
+
+    //Create model class
+    class Product
+    {
+        public int ID {get; set;}
+        public string Name {get; set;}
+        public double Price {get; set;}
+    }
+}
